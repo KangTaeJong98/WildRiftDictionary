64a96a41efa90ea019f7d97efa2c869c627373e4
Admin <rkdxowhd98@naver.com>
2021-02-11 07:35:48 +0900

    RuneFragment 구현완료

--- app/src/main/java/com/taetae98/wildriftdictionary/fragment/RuneFragment.kt
+++ app/src/main/java/com/taetae98/wildriftdictionary/fragment/RuneFragment.kt
@@ -1,32 +1,91 @@
 package com.taetae98.wildriftdictionary.fragment
 
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.google.android.flexbox.JustifyContent
+import com.taetae98.wildriftdictionary.GridSpacingItemDecoration
 import com.taetae98.wildriftdictionary.R
 import com.taetae98.wildriftdictionary.adapter.RuneAdapter
 import com.taetae98.wildriftdictionary.base.BaseFragment
+import com.taetae98.wildriftdictionary.data.Rune
 import com.taetae98.wildriftdictionary.data.RuneData
 import com.taetae98.wildriftdictionary.databinding.FragmentRuneBinding
+import com.taetae98.wildriftdictionary.toDp
 
 class RuneFragment : BaseFragment<FragmentRuneBinding>(R.layout.fragment_rune) {
+    private val onClick by lazy { { rune: Rune -> findNavController().navigate(RuneFragmentDirections.actionRuneFragmentToRuneDialog(rune))} }
+    private val gridSpacingItemDecoration by lazy { GridSpacingItemDecoration(1, 10.toDp(), RecyclerView.HORIZONTAL) }
+
     override fun init() {
         super.init()
         initSupportActionBar()
-        initRecyclerView()
+        initKeyStoneRecyclerView()
+        initDominationRecyclerView()
+        initResolveRecyclerView()
+        initInspirationRecyclerView()
     }
 
     private fun initSupportActionBar() {
         setSupportActionBar(binding.toolbar)
     }
 
-    private fun initRecyclerView() {
-        with(binding.recyclerView) {
+    private fun initKeyStoneRecyclerView() {
+        with(binding.keyStoneRecyclerView) {
             adapter = RuneAdapter().apply {
-                submitList(RuneData.getInstance().runes)
+                onClick = this@RuneFragment.onClick
+                layoutManager = FlexboxLayoutManager(requireContext()).apply {
+                    justifyContent = JustifyContent.CENTER
+                }
+                submitList(RuneData.getInstance().runes.filter {
+                    it.type == Rune.Type.KEY_STONE
+                })
+                addItemDecoration(gridSpacingItemDecoration)
             }
+        }
+    }
 
-            layoutManager = FlexboxLayoutManager(requireContext()).apply {
-                justifyContent = JustifyContent.CENTER
+    private fun initDominationRecyclerView() {
+        with(binding.dominationRecyclerView) {
+            adapter = RuneAdapter().apply {
+                onClick = this@RuneFragment.onClick
+                layoutManager = FlexboxLayoutManager(requireContext()).apply {
+                    justifyContent = JustifyContent.CENTER
+                }
+                submitList(RuneData.getInstance().runes.filter {
+                    it.type == Rune.Type.DOMINATION
+                })
+                addItemDecoration(gridSpacingItemDecoration)
+            }
+        }
+    }
+
+    private fun initResolveRecyclerView() {
+        with(binding.resolveRecyclerView) {
+            adapter = RuneAdapter().apply {
+                onClick = this@RuneFragment.onClick
+                layoutManager = FlexboxLayoutManager(requireContext()).apply {
+                    justifyContent = JustifyContent.CENTER
+                }
+                submitList(RuneData.getInstance().runes.filter {
+                    it.type == Rune.Type.RESOLVE
+                })
+                addItemDecoration(gridSpacingItemDecoration)
+            }
+        }
+    }
+
+    private fun initInspirationRecyclerView() {
+        with(binding.inspirationRecyclerView) {
+            adapter = RuneAdapter().apply {
+                onClick = this@RuneFragment.onClick
+                layoutManager = FlexboxLayoutManager(requireContext()).apply {
+                    justifyContent = JustifyContent.CENTER
+                }
+                submitList(RuneData.getInstance().runes.filter {
+                    it.type == Rune.Type.INSPIRATION
+                })
+                addItemDecoration(gridSpacingItemDecoration)
             }
         }
     }
